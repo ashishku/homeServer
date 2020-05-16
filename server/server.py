3db3e9d7ea7d4d3223b122a79e3a43e7e56b9395
Ashish Kumar <varma.ashishkumar@gmail.com>
2020-05-17 01:29:42 +0530

    fix json

--- server/server.py
+++ server/server.py
@@ -31,10 +31,11 @@ def switches():
     payload = []
     for room in rooms:
         _room = {
-            "label": rooms[room]["label"]
+            "label": rooms[room]["label"],
+            "switches": {}
         }
         for switch in rooms[room]["switches"]:
-            _room[switch] = {
+            _room["switches"][switch] = {
                 "label": get_switch_attr(room, switch, "label"),
                 "type": get_switch_attr(room, switch, "type"),
                 "on": GPIO.input(get_switch_attr(room, switch)) == 1
